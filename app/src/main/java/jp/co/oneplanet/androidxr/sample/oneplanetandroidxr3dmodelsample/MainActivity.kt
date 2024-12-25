5c9b17045b32445b35cce2b7e38dfdf416162fd5
tokufxug <tokufxug@gmail.com>
2024-12-26 00:33:45 +0900

    Initial commit

---
+++ app/src/main/java/jp/co/oneplanet/androidxr/sample/oneplanetandroidxr3dmodelsample/MainActivity.kt
@@ -0,0 +1,201 @@
+package jp.co.oneplanet.androidxr.sample.oneplanetandroidxr3dmodelsample
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.xr.compose.platform.LocalSession
+import androidx.xr.compose.platform.LocalHasXrSpatialFeature
+import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.spatial.EdgeOffset
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterEdge
+import androidx.xr.compose.spatial.Subspace
+import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.layout.SpatialRoundedCornerShape
+import androidx.xr.compose.subspace.layout.SubspaceModifier
+import androidx.xr.compose.subspace.layout.height
+import androidx.xr.compose.subspace.layout.movable
+import androidx.xr.compose.subspace.layout.resizable
+import androidx.xr.compose.subspace.layout.width
+import androidx.xr.runtime.math.Pose
+import androidx.xr.runtime.math.Vector3
+import androidx.xr.scenecore.SpatialCapabilities
+import jp.co.oneplanet.androidxr.sample.oneplanetandroidxr3dmodelsample.ui.theme.OnePlanetAndroidXR3DModelSampleTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            OnePlanetAndroidXR3DModelSampleTheme {
+                val session = LocalSession.current
+                if (LocalSpatialCapabilities.current.isSpatialUiEnabled) {
+                    Subspace {
+                        MySpatialContent(onRequestHomeSpaceMode = { session?.requestHomeSpaceMode() })
+                    }
+                } else {
+                    My2DContent(onRequestFullSpaceMode = { session?.requestFullSpaceMode() })
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun MySpatialContent(onRequestHomeSpaceMode: () -> Unit) {
+    SpatialPanel(SubspaceModifier.width(1280.dp).height(800.dp).resizable().movable()) {
+        Surface {
+            MainContent()
+        }
+        Orbiter(
+            position = OrbiterEdge.Top,
+            offset = EdgeOffset.inner(offset = 20.dp),
+            alignment = Alignment.End,
+            shape = SpatialRoundedCornerShape(CornerSize(28.dp))
+        ) {
+            HomeSpaceModeIconButton(
+                onClick = onRequestHomeSpaceMode,
+                modifier = Modifier.size(56.dp)
+            )
+        }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun My2DContent(onRequestFullSpaceMode: () -> Unit) {
+    Surface {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            MainContent()
+            if (LocalHasXrSpatialFeature.current) {
+                FullSpaceModeIconButton(
+                    onClick = onRequestFullSpaceMode,
+                    modifier = Modifier.padding(32.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Box(
+        Modifier
+            .background(color = Color.LightGray)
+            .height(1280.dp)
+            .width(800.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        ModelViewButton()
+    }
+}
+
+@Composable
+fun ModelViewButton() {
+    val xrSession = checkNotNull(LocalSession.current)
+    Button(onClick = {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                xrSession.requestFullSpaceMode()
+                val gltfModelFuture = withContext(Dispatchers.Main) {
+                    xrSession.createGltfResourceAsync("models/CesiumMan.glb")
+                }
+                val gltfModel = gltfModelFuture.get()
+                withContext(Dispatchers.Main) {
+                    if (xrSession.getSpatialCapabilities().hasCapability(SpatialCapabilities.SPATIAL_CAPABILITY_3D_CONTENT)) {
+                        val gltfEntity = xrSession.createGltfEntity(gltfModel)
+                        val modelPosition = Vector3(0f, -1f, 0f)
+                        gltfEntity.setPose(Pose(modelPosition))
+                        gltfEntity.setScale(1f)
+                        gltfEntity.startAnimation(true)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }) {
+        Text(text = "3Dモデル表示")
+    }
+}
+
+@Composable
+fun FullSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_full_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_full_space_mode)
+        )
+    }
+}
+
+@Composable
+fun HomeSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    FilledTonalIconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_home_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_home_space_mode)
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun My2dContentPreview() {
+    OnePlanetAndroidXR3DModelSampleTheme {
+        My2DContent(onRequestFullSpaceMode = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullSpaceModeButtonPreview() {
+    OnePlanetAndroidXR3DModelSampleTheme {
+        FullSpaceModeIconButton(onClick = {})
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun HomeSpaceModeButtonPreview() {
+    OnePlanetAndroidXR3DModelSampleTheme {
+        HomeSpaceModeIconButton(onClick = {})
+    }
+}
